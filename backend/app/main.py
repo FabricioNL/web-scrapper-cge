08bd845ad8c8fff3e8478d2d903daf2d3cc5ecf1
Fabricio2530 <fabricio.neri.ismart@gmail.com>
2025-01-11 23:00:49 -0300

    fix: CORS policy

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.routers import floods
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Configuração de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todas as origens. Use uma lista específica para restringir.
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos HTTP: GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],  # Permite todos os cabeçalhos.
+)
 
 app.include_router(floods.router, prefix="/api", tags=["floods"])
 
